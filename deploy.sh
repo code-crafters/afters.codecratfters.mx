eceddd105a3f6ee717296b64bb3d9c46d296af4f
Fernando Flores <ferflores@gmail.com>
2020-08-29 08:19:10 -0500

    vue deploy

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ printf 'afters.codecrafters.mx' > CNAME | tr -d '\n\r'
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com/code-crafters.git master:gh-pages
+git push -f https://github.com/code-crafters/afters.codecratfters.mx.git master:gh-pages 
 cd -
