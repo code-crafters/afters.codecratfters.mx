9b283efe0aa2aa7a365c3e40bf60699bfc40223d
Fernando Flores <ferflores@gmail.com>
2020-08-31 09:25:37 -0500

    deployment test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/cca_logo.png">
     <HelloWorld msg="Code Crafters Afters Vue.js App"/>
   </div>
 </template>
